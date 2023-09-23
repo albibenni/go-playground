8c9044182c9c08381d9dd40f61c102694b50442d
Alberto Benatti <albi.benni8@gmail.com>
2023-09-23 21:49:14 +0200

    util function

---
+++ src/utils/os.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func PrintOsArgs () {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
