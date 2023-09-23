f96ac04919901c0cb03d08539787f4db91bec6c7
Alberto Benatti <albi.benni8@gmail.com>
2023-09-23 21:49:07 +0200

    refactor functions

--- src/main.go
+++ src/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"main/src/utils"
 )
-func printOsArgs () {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
-}
 
 func main() {
-	println("main running")
-	printOsArgs()
-}   
+	fmt.Println("main running")
+	utils.PrintOsArgs()
+}
